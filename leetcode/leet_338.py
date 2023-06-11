53531c10afb1c6d34563a386158a80a01dea08e8
dynstat <vs15vivek@gmail.com>
2023-06-11 10:32:45 +0530

    leet-348 solution added

---
+++ leetcode/leet_338.py
@@ -0,0 +1,29 @@
+# Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+
+# Example 1:
+
+# Input: n = 2
+# Output: [0,1,1]
+# Explanation:
+# 0 --> 0
+# 1 --> 1
+# 2 --> 10
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        def bitcount(num):
+            return bin(num)[2:].count("1")
+
+        bitcount_list = [-1] * 100000
+        out = []
+        for i in range(n + 1):
+            bc = -1
+            if bitcount_list[i] == -1:
+                bc = bitcount(i)  # this is a function which might take time
+                bitcount_list.append(bc)
+            else:
+                bc = bitcount_list[i]  # O(1)
+            out.append(bc)
+        return out
