46750f5749a26742c0716d048616aa32bc393520
dynstat <vs15vivek@gmail.com>
2023-06-15 20:33:02 +0530

    testing example without using external global variable

--- leetcode/leet_110.py
+++ leetcode/leet_110.py
@@ -13,25 +13,65 @@
 
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
+#     def __init__(self, val=0, ):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    ans = True
 
-    def rec(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return False
 
-        l = self.rec(root.left)
-        r = self.rec(root.right)
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-        if (l - r) > 1 or (r - l) > 1:
-            self.ans = False
+    def __repr__(self):
+        return f"Node({self.val})"
 
-        return max(l, r) + 1
+    def __str__(self):
+        return str(self.val)
 
-    def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        self.rec(root)
-        return self.ans
+
+# class Solution:
+#     ans = True
+
+#     def rec(self, root: Optional[TreeNode]) -> bool:
+#         if not root:
+#             return False
+
+#         l = self.rec(root.left)
+#         r = self.rec(root.right)
+
+#         if (l - r) > 1 or (r - l) > 1:
+#             self.ans = False
+
+#         return max(l, r) + 1
+
+#     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+#         self.rec(root)
+#         return self.ans
+
+
+#############################################################################  testing other related stuff ########################################
+# creating a Tree
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7, TreeNode(9), TreeNode(2))
+
+
+def rec(root, ans=True):
+    if not root:
+        return 0, True
+
+    l, ans = rec(root.left, ans)
+    r, ans = rec(root.right, ans)
+
+    if (l - r) > 1 or (r - l) > 1:
+        ans = False
+
+    return ((max(l, r) + 1), ans)
+
+
+print(rec(root))
