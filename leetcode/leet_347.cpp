14f8535d5fd843178b0d5a8de508de5cab399055
dynstat <vs15vivek@gmail.com>
2024-02-23 07:53:27 +0530

    leetcode 347 (cpp)

--- leetcode/leet_347.cpp
+++ leetcode/leet_347.cpp
@@ -24,89 +24,82 @@
 // Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
 
 #include <vector>
-#include <queue>
 #include <unordered_map>
 #include <iostream>
-
-std::vector<int> topKFrequent(std::vector<int> &nums, int k)
+using namespace std;
+struct MyStruct
 {
-    std::unordered_map<int, int> freqMap;
-    for (int num : nums)
-    {
-        freqMap[num]++;
-    }
-
-    // Define a lambda function 'compare' that takes two pairs of integers as input parameters
-    // The lambda function compares the second element of the pairs and returns true if the second element of the first pair is less than the second element of the second pair
-    auto compare = [](const std::pair<int, int> &a, const std::pair<int, int> &b)
-    {
-        return a.second < b.second;
-    };
-
-    // Create a priority queue 'pq' of pairs of integers, with the second integer in the pair being the priority value
-    // Use the 'compare' lambda function to compare the second elements of the pairs
-    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, decltype(compare)> pq(compare);
-
-    for (auto &entry : freqMap)
-    {
-        pq.push(entry);
-        if (pq.size() > k)
-        {
-            pq.pop();
-        }
-    }
-
-    std::vector<int> result;
-    while (!pq.empty())
-    {
-        result.push_back(pq.top().first);
-        pq.pop();
-    }
-
-    return result;
-}
-
-std::vector<int> topKFrequent2(std::vector<int> &nums, int k)
+    int intValue;
+    char charValue;
+    double doubleValue;
+};
+class Solution
 {
-    std::unordered_map<int, int> count_map;
+public:
+    vector<int> topKFrequent(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> count_map;
+        vector<vector<int>> ans(nums.size() + 1);
+        vector<int> final;
+        for (int num : nums)
+        {
+            try
+            {
+                count_map[num] += 1;
+            }
+            catch (...)
+            {
+                count_map[num] = 1;
+            }
+        }
 
-    for (int num : nums)
-    {
-        try
+        for (const auto &pair : count_map)
         {
-            count_map[num] += 1;
+            int key = pair.first;
+            int val = pair.second;
+
+            ans[val].push_back(key);
         }
-        catch (...)
+        for (auto subarr = ans.rbegin(); subarr != ans.rend(); subarr++)
         {
-            count_map[num] = 1;
+            for (int num : *subarr)
+            {
+                final.push_back(num);
+                k--;
+                if (k <= 0)
+                {
+                    return final;
+                }
+            }
         }
+        // for (auto &subarr : ans)
+        // {
+        //     for (int num : subarr)
+        //     {
+        //         final.push_back(num);
+        //         k--;
+        //         if (k <= 0)
+        //         {
+        //             break;
+        //         }
+        //     }
+        // }
+
+        return final;
     }
-    int max = 0;
-    int second_max = 0;
-    for (auto &pair : count_map)
-    {
-        if (pair.second > max)
-        {
-            second_max = max;
-            max = pair.first;
-        }
-    }
-    std::vector<int> r = {2, 3};
-    return r;
-}
+};
 
 int main()
 {
-    std::vector<int> nums = {1, 1, 1, 2, 2, 3};
+    Solution sol;
+    // vector<int> nums = {1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 5, 5, 5, 5};
+    // vector<int> nums = {1, 2};
+    vector<int> nums = {4, 1, -1, 2, -1, 2, 3};
     int k = 2;
-
-    std::vector<int> result = topKFrequent(nums, k);
-
-    for (int num : result)
+    vector<int> ans = sol.topKFrequent(nums, k);
+    for (int num : ans)
     {
-        std::cout << num << " ";
+        cout << num << " ";
     }
-    std::cout << std::endl;
-
     return 0;
 }
